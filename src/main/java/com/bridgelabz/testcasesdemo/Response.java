feca51377f6dc7e725c8797bc8362aef7e57cda7
sidgharge <siddharth.gharge@bridgelabz.com>
2018-08-11 18:36:57 +0530

    Sample

---
+++ src/main/java/com/bridgelabz/testcasesdemo/Response.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.testcasesdemo;
+
+public class Response {
+
+	private String message;
+
+	private int status;
+
+	public Response() {
+
+	}
+
+	public Response(String message, int status) {
+		this.message = message;
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Response [message=" + message + ", status=" + status + "]";
+	}
+
+}
